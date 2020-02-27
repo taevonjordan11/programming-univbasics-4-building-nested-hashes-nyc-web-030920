bada05f8583d58a86a01fc1817a9c31e7f8091f9
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-27 04:52:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,7 +48,7 @@ def monopoly_with_fourth_tier
 	    names: {
 	      shortline_railroad: {},
 	      b_and_o_railroad: {}, 
-	      pennsylvania_railroad:{},
+	      pennsylvania_railroad:{"mortgage_value" => "$200"},
 	      reading_railroad: {"mortgage_value" => "$100"}
 	    }
 	  }
